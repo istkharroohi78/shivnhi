ee8f4fa29e731ac0761c111e25e522254dea22e5
THE SHIV <shivofficial004@gmail.com>
2026-06-15 23:57:00 +0530

    Update ping.py

--- PritiMusic/plugins/tools/ping.py
+++ PritiMusic/plugins/tools/ping.py
@@ -1,42 +1,44 @@
-import random  # <--- YEH IMPORT JARURI HAI
+import random 
 from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
-from config import *
+
 from PritiMusic import app
 from PritiMusic.core.call import Lucky
 from PritiMusic.utils import bot_sys_stats
 from PritiMusic.utils.decorators.language import language
 from PritiMusic.utils.inline import supp_markup
-from config import BANNED_USERS
-from config import PING_IMG_URL
+from config import BANNED_USERS, PING_IMG_URL
 
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(
+    filters.command(["ping", "alive"], prefixes=["/", "!", "#"]) 
+    & ~BANNED_USERS
+)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     
-    # --- FIX START ---
-    # Agar PING_IMG_URL list hai (split laga hai), toh random choose karo
-    # Agar single string hai, toh waisa hi rahne do
+    # --- RANDOM IMAGE LOGIC ---
     if isinstance(PING_IMG_URL, list):
         response_img = random.choice(PING_IMG_URL)
     else:
         response_img = PING_IMG_URL
-    # --- FIX END ---
 
+    # Pehle loading wala photo caption bhejenge
     response = await message.reply_photo(
-        photo=response_img, # <--- Yahan fixed variable use kiya
+        photo=response_img, 
         caption=_["ping_1"].format(app.mention),
     )
     
+    # Stats calculate kar rahe hain
     pytgping = await Lucky.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+    # 🟢 THE FIX: Photo ke text ko badalne ke liye 'edit_caption' use hota hai
+    await response.edit_caption(
+        caption=_["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),
     )
